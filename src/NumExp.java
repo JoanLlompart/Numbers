0d07b84820694d17cd8687050936377b920789ab
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2023-06-05 10:35:55 +0200

    Antes de canvi radical

--- src/NumExp.java
+++ src/NumExp.java
@@ -20,7 +20,6 @@ public class NumExp {
 
          */
     }
-
     public static String say(long n) {
         //La funció “say” acceptarà un paràmetre de tipus “long” i tornarà un String amb les paraules
         //en català corresponents al número que li hem passat.
